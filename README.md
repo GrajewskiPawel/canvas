7484889eb746bdb12947bbde5239b334a204fd07
GrajewskiPawel <pawel@pawelgrajewski.com.pl>
2017-10-26 23:50:43 +0200

    drawing in canvas

---
+++ README.md
@@ -0,0 +1,7 @@
+#Project Explanation
+
+Simple example of use canvas technology in small project
+
+##Languages use in project
+
+html
---
+++ js/script.js
@@ -0,0 +1,21 @@
+window.onload = function(){
+var canvas = document.getElementById("myCanvas");	
+	var ctx = canvas.getContext("2d");
+	ctx.fillStyle = "#FF0000";
+	ctx.fillRect(0,0,150,75);
+	ctx.strokeStyle='blue';
+	ctx.moveTo(0,0);
+	ctx.lineTo(200,100);
+	ctx.stroke();
+	ctx.beginPath();
+	ctx.arc(95,50,40,0,2*Math.PI);
+	ctx.stroke();
+	var grd = ctx.createLinearGradient(0,0,200,0);
+	grd.addColorStop(0, "red");
+	grd.addColorStop(1,"white");
+	ctx.fillStyle = grd;
+	ctx.fillRect(10,10,150,80);
+	ctx.fillStyle = "#000000";
+	ctx.font = "30px Arial";
+	ctx.fillText("Hello World",10,50);
+};

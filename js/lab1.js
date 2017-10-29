2575acd6dcdb8dde8c8af94c17ce4707d2b85084
GrajewskiPawel <pawel@pawelgrajewski.com.pl>
2017-10-29 22:48:57 +0100

    lab1 step 2

--- js/lab1.js
+++ js/lab1.js
@@ -1,3 +1,9 @@
 window.onload = function(){
-	 
+	 var canvas = document.getElementById("lab1");
+	
+	var ctx = canvas.getContext("2d");
+	ctx.beginPath();
+	ctx.moveTo(20,20);
+	ctx.quadraticCurveTo(20,100,200,20);
+	ctx.stroke();
 };

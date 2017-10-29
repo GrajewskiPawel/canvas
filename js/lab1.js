ab73d0b7b83abe4263b88298ff8f33605392819e
GrajewskiPawel <pawel@pawelgrajewski.com.pl>
2017-10-29 23:11:20 +0100

    step3

--- js/lab1.js
+++ js/lab1.js
@@ -1,9 +1,31 @@
 window.onload = function(){
 	 var canvas = document.getElementById("lab1");
-	
 	var ctx = canvas.getContext("2d");
+	
+	
+	var gradient = ctx.createLinearGradient(0,0,170,0);
+	gradient.addColorStop("0","magenta");
+	gradient.addColorStop("0.5","blue");
+	gradient.addColorStop("1.0","red");
+	
 	ctx.beginPath();
 	ctx.moveTo(20,20);
-	ctx.quadraticCurveTo(20,100,200,20);
+	ctx.quadraticCurveTo(20,200,200,20);
+	ctx.strokeStyle="#FF0000";
+	ctx.lineWidth = 5;
+	ctx.stroke();
+	
+	ctx.beginPath();
+	ctx.moveTo(20,30);
+	ctx.quadraticCurveTo(30,200,200,30);
+	ctx.strokeStyle=gradient;
+	ctx.lineWidth = 5;
+	ctx.stroke();
+	
+	ctx.beginPath();
+	ctx.moveTo(20,20);
+	ctx.lineTo(20,100);
+	ctx.lineTo(70,100);
+	ctx.closePath();
 	ctx.stroke();
 };
